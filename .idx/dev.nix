2e68e708b7dd4158970ae2e8844717768a66169f
cowleyy <cowleyy@gmail.com>
2025-07-13 01:32:07 +0000

    11

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,8 +1,8 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://firebase.google.com/docs/studio/customize-workspace
 { pkgs, ... }: {
-  # Which nixpkgs channel to use.
-  channel = "stable-24.05"; # or "unstable"
+  # Který kanál nixpkgs použít.
+  channel = "stable-24.05"; # or "unstable"Â 
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
