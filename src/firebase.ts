09b07e2fdc1d1e8435f41a123ba0e3f1a9780f61
Cowley <159197257+CowleyCZE@users.noreply.github.com>
2025-07-19 00:39:53 +0000

    Odstranění nepoužívaných importů a zakomentování inicializace analytiky ve firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -19,13 +19,10 @@ export const auth = getAuth(firebaseApp);
 export const appId = firebaseConfig.appId;
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(firebaseApp);
