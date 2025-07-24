cd7188519f389cb4c56daf49aa31091144d7b65f
Cowley <159197257+CowleyCZE@users.noreply.github.com>
2025-07-24 15:44:55 +0000

    Add testing scripts and dependencies for Jest and Testing Library
    - Added "test" script to package.json for running Jest tests.
    - Included Jest and related packages in devDependencies for testing support.
    - Added @testing-library/jest-dom and @testing-library/react for enhanced testing capabilities.

---
+++ jest.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: 'tsconfig.json',
+    }],
+  },
+};
